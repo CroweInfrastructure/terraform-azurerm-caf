1811177162e113226affcdaba249c0153057c660
syedmdanish <syed.mohd.danish@gmail.com>
2022-11-17 19:18:00 +0530

    Update locals.tf
    Test certificate creation

--- locals.tf
+++ locals.tf
@@ -8,16 +8,17 @@ resource "random_string" "prefix" {
 
 locals {
   azuread = {
-    azuread_api_permissions             = try(var.azuread.azuread_api_permissions, {})
-    azuread_applications                = try(var.azuread.azuread_applications, {})
-    azuread_apps                        = try(var.azuread.azuread_apps, {})
-    azuread_credential_policies         = try(var.azuread.azuread_credential_policies, {})
-    azuread_credentials                 = try(var.azuread.azuread_credentials, {})
-    azuread_groups                      = try(var.azuread.azuread_groups, {})
-    azuread_groups_membership           = try(var.azuread.azuread_groups_membership, {})
-    azuread_roles                       = try(var.azuread.azuread_roles, {})
-    azuread_service_principal_passwords = try(var.azuread.azuread_service_principal_passwords, {})
-    azuread_service_principals          = try(var.azuread.azuread_service_principals, {})
+    azuread_api_permissions               = try(var.azuread.azuread_api_permissions, {})
+    azuread_applications                  = try(var.azuread.azuread_applications, {})
+    azuread_apps                          = try(var.azuread.azuread_apps, {})
+    azuread_credential_policies           = try(var.azuread.azuread_credential_policies, {})
+    azuread_credentials                   = try(var.azuread.azuread_credentials, {})
+    azuread_groups                        = try(var.azuread.azuread_groups, {})
+    azuread_groups_membership             = try(var.azuread.azuread_groups_membership, {})
+    azuread_roles                         = try(var.azuread.azuread_roles, {})
+    azuread_service_principal_passwords   = try(var.azuread.azuread_service_principal_passwords, {})
+    azuread_service_principals            = try(var.azuread.azuread_service_principals, {})
+    azuread_graph_application_certificate = try(var.azuread.azuread_graph_application_certificate, {})
 
     azuread_administrative_units                          = try(var.azuread.azuread_administrative_units, {})
     azuread_administrative_unit_members                   = try(var.azuread.azuread_administrative_unit_members, {})
