ba4da4ed2cf578dc98ced1cb54c4883354a6b3f1
syedmdanish <syed.mohd.danish@gmail.com>
2022-11-18 16:41:27 +0530

    Update module.tf
    Test oauth2_permission_scope

--- modules/azuread_graph/application/module.tf
+++ modules/azuread_graph/application/module.tf
@@ -7,12 +7,12 @@ data "azuread_application_template" "template" {
 resource "azuread_application" "app" {
   display_name = try(var.settings.display_name, var.settings.application_name, null)
   dynamic "api" {
-    for_each = try(var.settings.api, null) != null ? [var.settings.api] : []
+    for_each = try(var.settings.api, null) != null ? var.settings.api : []
     content {
       known_client_applications = try(api.value.known_client_applications, null)
       mapped_claims_enabled     = try(api.value.mapped_claims_enabled, null)
       dynamic "oauth2_permission_scope" {
-        for_each = try(api.value.oauth2_permission_scope, null) != null ? [api.value.oauth2_permission_scope] : []
+        for_each = try(api.value.oauth2_permission_scope, null) != null ? api.value.oauth2_permission_scope : []
         content {
           id                         = oauth2_permission_scope.value.id
           admin_consent_description  = try(oauth2_permission_scope.value.admin_consent_description, null)
