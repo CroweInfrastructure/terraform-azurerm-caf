0de14c1c1c12eb4f229c864896954ac69db28799
syedmdanish <syed.mohd.danish@gmail.com>
2022-12-07 19:11:03 +0530

    Update module.tf
    Update application_object_id to use the object_id of application

--- modules/azuread_graph/application_password/module.tf
+++ modules/azuread_graph/application_password/module.tf
@@ -1,7 +1,7 @@
 data "azuread_application_published_app_ids" "well_known" {}
 
 resource "azuread_application_password" "appp" {
-  application_object_id = can(var.settings.azuread_application_object.key) == false ? try(var.settings.azuread_application_object.id, data.azuread_application_published_app_ids.well_known.result[var.settings.azuread_application.well_known_key]) : var.remote_objects.azuread_applications[try(var.settings.azuread_application_object.lz_key, var.client_config.landingzone_key)][var.settings.azuread_application_object.key].application_id
+  application_object_id = can(var.settings.azuread_application_object.key) == false ? try(var.settings.azuread_application_object.id, data.azuread_application_published_app_ids.well_known.result[var.settings.azuread_application.well_known_key]) : var.remote_objects.azuread_applications[try(var.settings.azuread_application_object.lz_key, var.client_config.landingzone_key)][var.settings.azuread_application_object.key].object_id
   display_name          = try(var.settings.display_name, null)
   start_date            = try(var.settings.start_date, null)
   end_date              = try(var.settings.end_date, null)
