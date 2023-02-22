9540b0b12b8c9d00cf2b3fd7d770e0d034a84612
Jonathan Alexandre <jondre16@gmail.com>
2023-02-22 20:37:20 +0000

    wip

--- modules/webapps/appservice/module.tf
+++ modules/webapps/appservice/module.tf
@@ -272,21 +272,19 @@ resource "azurerm_app_service" "app_service" {
   ###########################################################
   ### BEGIN CROWE_PBI_226389
   ###########################################################
-  # var.app_service_lifecycle_ignore_changes1 = merge([
-  #    app_settings["WEBSITE_RUN_FROM_PACKAGE"],
-  #    site_config[0].scm_type
-  #  ], var.app_service_lifecycle_ignore_changes)
+  lifecycle {
+    ignore_changes = concat([
+      app_settings["WEBSITE_RUN_FROM_PACKAGE"],
+      site_config[0].scm_type
+    ], var.app_service_lifecycle_ignore_changes)
+  }
 
   # lifecycle {
-  #  ignore_changes = var.app_service_lifecycle_ignore_changes1   
+  #   ignore_changes = [
+  #     app_settings["WEBSITE_RUN_FROM_PACKAGE"],
+  #     site_config[0].scm_type
+  #   ]
   # }
-
-   lifecycle {
-     ignore_changes = [
-       app_settings["WEBSITE_RUN_FROM_PACKAGE"],
-       site_config[0].scm_type
-     ]
-   }
   ###########################################################
   ### END CROWE_PBI_226389
   ###########################################################
