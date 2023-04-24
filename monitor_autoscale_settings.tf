395f3a900041aacff7b82126ab1867ede3267e46
syedmdanish <syed.mohd.danish@gmail.com>
2023-04-24 12:54:58 +0530

    Tags - Update Check Fix

--- monitor_autoscale_settings.tf
+++ monitor_autoscale_settings.tf
@@ -2,6 +2,7 @@ module "monitor_autoscale_settings" {
   depends_on          = [module.virtual_machine_scale_sets, module.app_service_plans]
   source              = "./modules/monitoring/monitor_autoscale_settings"
   for_each            = local.shared_services.monitor_autoscale_settings
+  base_tags           = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
   settings            = each.value
   global_settings     = local.global_settings
   client_config       = local.client_config
